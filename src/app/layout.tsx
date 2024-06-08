046bd5e097fe88901574e240bc1041a52bfb5a84
drmatt13 <Matthew.Sweeney001@gmail.com>
2024-06-07 23:46:04 -0400

    6/7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ColorStream Dynamics: Documentation",
+  title: "GlobalChat: Documentation",
 };
 
 export default function RootLayout({
